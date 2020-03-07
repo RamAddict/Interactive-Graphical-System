fd73f08eba869cb299d5ade9d76741119035dce9
Gabriel B. Sant'Anna <baiocchi.gabriel@gmail.com>
2020-03-07 09:45:34 -0300

    Test PySide Qt API

---
+++ sgi/gui.py
@@ -0,0 +1,65 @@
+# -*- coding: utf-8 -*-
+
+################################################################################
+## Form generated from reading UI file 'g.ui'
+##
+## Created by: Qt User Interface Compiler version 5.14.1
+##
+## WARNING! All changes made in this file will be lost when recompiling UI file!
+################################################################################
+
+from PySide2.QtCore import (QCoreApplication, QMetaObject, QObject, QPoint,
+    QRect, QSize, QUrl, Qt)
+from PySide2.QtGui import (QBrush, QColor, QConicalGradient, QCursor, QFont,
+    QFontDatabase, QIcon, QLinearGradient, QPalette, QPainter, QPixmap,
+    QRadialGradient)
+from PySide2.QtWidgets import *
+
+
+class Ui_MainWindow(object):
+    def setupUi(self, MainWindow):
+        if MainWindow.objectName():
+            MainWindow.setObjectName(u"MainWindow")
+        MainWindow.resize(805, 601)
+        self.centralwidget = QWidget(MainWindow)
+        self.centralwidget.setObjectName(u"centralwidget")
+        self.main_vie_port = QGraphicsView(self.centralwidget)
+        self.main_vie_port.setObjectName(u"main_vie_port")
+        self.main_vie_port.setGeometry(QRect(-10, -10, 591, 421))
+        self.up_btn = QPushButton(self.centralwidget)
+        self.up_btn.setObjectName(u"up_btn")
+        self.up_btn.setGeometry(QRect(670, 210, 41, 25))
+        self.left_btn = QPushButton(self.centralwidget)
+        self.left_btn.setObjectName(u"left_btn")
+        self.left_btn.setGeometry(QRect(630, 240, 41, 25))
+        self.right_btn = QPushButton(self.centralwidget)
+        self.right_btn.setObjectName(u"right_btn")
+        self.right_btn.setGeometry(QRect(710, 240, 41, 25))
+        self.down_btn = QPushButton(self.centralwidget)
+        self.down_btn.setObjectName(u"down_btn")
+        self.down_btn.setGeometry(QRect(670, 270, 41, 25))
+        self.textBrowser_2 = QTextBrowser(self.centralwidget)
+        self.textBrowser_2.setObjectName(u"textBrowser_2")
+        self.textBrowser_2.setGeometry(QRect(0, 430, 591, 171))
+        self.horizontalSlider = QSlider(self.centralwidget)
+        self.horizontalSlider.setObjectName(u"horizontalSlider")
+        self.horizontalSlider.setGeometry(QRect(610, 310, 160, 16))
+        self.horizontalSlider.setOrientation(Qt.Horizontal)
+        self.display_file = QListWidget(self.centralwidget)
+        self.display_file.setObjectName(u"display_file")
+        self.display_file.setGeometry(QRect(600, 10, 191, 171))
+        MainWindow.setCentralWidget(self.centralwidget)
+
+        self.retranslateUi(MainWindow)
+
+        QMetaObject.connectSlotsByName(MainWindow)
+    # setupUi
+
+    def retranslateUi(self, MainWindow):
+        MainWindow.setWindowTitle(QCoreApplication.translate("MainWindow", u"MainWindow", None))
+        self.up_btn.setText(QCoreApplication.translate("MainWindow", u"PushButton", None))
+        self.left_btn.setText(QCoreApplication.translate("MainWindow", u"PushButton", None))
+        self.right_btn.setText(QCoreApplication.translate("MainWindow", u"PushButton", None))
+        self.down_btn.setText(QCoreApplication.translate("MainWindow", u"PushButton", None))
+    # retranslateUi
+
