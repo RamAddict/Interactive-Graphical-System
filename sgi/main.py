46aabf657dc137748704e983608d52000cf6615c
Gabriel B. Sant'Anna <baiocchi.gabriel@gmail.com>
2020-03-07 16:10:28 -0300

    Settle Drawable data structures

--- sgi/main.py
+++ sgi/main.py
@@ -2,8 +2,21 @@
 
 import sys
 from PySide2.QtWidgets import QApplication, QMainWindow
+from PySide2.QtGui import QPainter
 
 from .gui import Ui_MainWindow
+from .graphics import *
+
+
+class MyPainter(Painter):
+    def __init__(self, widget):
+        self.painter = QPainter(widget)
+
+    def draw_pixel(self, x, y):
+        self.painter.drawPoint(x, y)
+
+    def draw_line(self, x1, y1, x2, y2):
+        self.painter.drawLine(x1, y1, x2, y2)
 
 
 class InteractiveGraphicalSystem(QMainWindow, Ui_MainWindow):
@@ -13,6 +26,15 @@ class InteractiveGraphicalSystem(QMainWindow, Ui_MainWindow):
         super(InteractiveGraphicalSystem, self).__init__()
         self.setupUi(self)
         self.show()
+
+        self.objects = {'line': Line(Point(500, 500), Point(700, 500)),
+                        'point': Point(250, 250),
+                        'wireframe': Wireframe(Point(0, 0), Point(300, 0), Point(300, 400))}
+
+    def paintEvent(self, event):
+        painter = MyPainter(self)
+        for name, obj in self.objects.items():
+            obj.draw(painter)
 
 
 if __name__ == '__main__':
