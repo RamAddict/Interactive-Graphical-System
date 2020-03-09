141dd0d143661da70978084022edc9b2705ee57a
Gabriel B. Sant'Anna <baiocchi.gabriel@gmail.com>
2020-03-08 21:25:57 -0300

    Improve IGS API
    Also added a Makefile

--- sgi/graphics.py
+++ sgi/graphics.py
@@ -1,29 +1,8 @@
 """Higher-level graphics API."""
 
-from primitives import Drawable, Point, Painter
 from typing import Tuple
 
-from util import pairwise
-
-
-class Wireframe(Drawable):
-    def __init__(self, *points: Tuple[Point]):
-        self.points = list(points)
-
-    def draw(self, painter):
-        for p1, p2 in pairwise(self.points + [self.points[0]]):
-            painter.draw_line(p1.x, p1.y, p2.x, p2.y)
-
-    def __getitem__(self, key):
-        return self.points[key]
-
-    def __setitem__(self, key, point: Point):
-        self.points[key] = point
-
-    def __str__(self):
-        return '({})'.format(
-            '; '.join(str(p) for p in self.points + [self.points[0]])
-        )
+from primitives import Painter, Point
 
 
 class Camera(Painter):
@@ -42,15 +21,17 @@ class Camera(Painter):
         x, y = self._transform(x - self.x, y - self.y)
         self.painter.draw_pixel(x, y)
 
-    def draw_line(self, x1, y1, x2, y2):
-        x1, y1 = self._transform(x1 - self.x, y1 - self.y)
-        x2, y2 = self._transform(x2 - self.x, y2 - self.y)
-        self.painter.draw_line(x1, y1, x2, y2)
+    def draw_line(self, xa, ya, xb, yb):
+        xa, ya = self._transform(xa - self.x, ya - self.y)
+        xb, yb = self._transform(xb - self.x, yb - self.y)
+        self.painter.draw_line(xa, ya, xb, yb)
 
     def _transform(self, x, y) -> Tuple:
-        """Apply the Window to Viewport Transformation on given coordinates."""
+        """Apply the Window-to-Viewport Transformation on given coordinates."""
+        # x: equivalent to lerp(x, x_min, x_min+width, 0, _viewport_width)
         x_min = self.x - self.width/2
         x = (x - x_min) * self._viewport_width / self.width
+        # _viewport_height - lerp(y, y_min, y_min+height, 0, _viewport_height)
         y_min = self.y - self.height/2
         y = self._viewport_height * (1 - (y - y_min)/self.height)
         return x, y
@@ -60,7 +41,7 @@ class Camera(Painter):
         return self._viewport_width / self.width
 
     @zoom.setter
-    def zoom(self, scale):
+    def zoom(self, scale: float):
         self.width = self._viewport_width / scale
         self.height = self._viewport_height / scale
 
--- sgi/util.py
+++ sgi/util.py
@@ -1,14 +1,14 @@
-"""Miscellaneous project utilities."""
+"""Miscellaneous python utilities."""
 
 from typing import Iterable, Sequence, Generator, Tuple
 from itertools import tee
+from math import log2
 
 
 def grouped(iterable: Iterable, n: int) -> Generator[Tuple, None, None]:
     """Goes through an Iterable, n elements at a time.
     If there aren't enough elements left for a last iteration, it won't happen.
     """
-
     it = iter(iterable)
     return zip(*([it] * n))
 
@@ -17,13 +17,29 @@ def pairwise(seq: Sequence) -> Generator[Tuple, None, None]:
     """Goes through a Sequence, a pair at a time with repetition.
     eg: [1, 2, 3, 4, 5] -> (1, 2), (2, 3), (3, 4), (4, 5)
     """
-
     first = iter(seq)
     first, second = tee(first)
     next(second)
-
     while True:
         try:
             yield next(first), next(second)
         except StopIteration:
             return
+
+
+def lerp(x, in_min, in_max, out_min, out_max):
+    """Linearly interpolate x in [in_min,in_max] to y in [out_min,out_max]."""
+    return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min
+
+
+def experp(x, in_min, in_max, out_min, out_max):
+    """Interpolate from a linear to an exponential range."""
+    # solving for y = a * exp(b * x), while knowing that
+    # x1 = in_min -> y1 = out_min and x2 = in_max -> y2 = out_max, thus
+    # y1/y2 = exp(b * x1) / exp(b * x2) = exp(b * (x1 - x2)), then taking the
+    # log of both sides: log(y1/y2) = b * (x1 - x2), so we have found b.
+    b = log2(out_min / out_max) / (in_min - in_max)
+    # similarly for a, y1 = a * exp(b * x1) -> a = y1 / exp(b * x1)
+    a = out_min / 2**(b * in_min)
+    # now we plug them into the equation (PS: base 2 is probably faster than e)
+    return a * 2**(b * x)
