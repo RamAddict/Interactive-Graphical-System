cf3b740d86dced7e6b01923938f9f6b9e0b596b8
RamAddict <apbiancofriber@gmail.com>
2021-07-01 21:18:38 -0300

    Implement save as obj functionality

--- pycg/obj.py
+++ pycg/obj.py
@@ -1,25 +1,68 @@
 
-from typing import Dict
-from graphics import Drawable
+from typing import Dict, Tuple
+from graphics import Drawable, Point, Line, Wireframe
 
 
 class ObjDescriptor:
     """Class for Writing .obj files"""
     def __init__(self, display_file: Dict[str, Drawable]):
         self.display_file = display_file
+        self.counter = 0; 
         # file.write("# List of vertices:")
 
-    def write_obj(self, file_name: str, object: str):
-        file = open(file_name, 'a')
-        for point in self.display_file[object]:
-            file.write("v " + str(point.x) + " " + str(point.y) + " 1.0 1.0\n")
+    def reset_counter(self):
+        self.counter = 0;
+
+    def write_obj(self, object_name: str, reset_counter=False) -> Tuple:
+        """
+        first string is list of vertices, second string is rest of object details
+        returns Tuple(a, b) where a is the vertices and b is the structure.
+        The return is ready to be written to a file
+        """
+        if reset_counter:
+            self.reset_counter();
+        drawable = self.display_file[object_name]
+        vertices = ""
+        old_counter = self.counter +1
+        if type(drawable) is Point:
+                vertices += ("v " + str(drawable.x) + " " + str(drawable.y) + " 0.0 1.0\n")
+                self.counter+=1
+        else:
+            for point in drawable:
+                vertices += ("v " + str(point.x) + " " + str(point.y) + " 0.0 1.0\n")
+                self.counter+=1
+
+        # define relevant attributes on the vertex (structure)
+        structure = "o " + object_name + "\n"
+        if type(drawable) is Point:
+            structure += "p " + str(self.counter) + "\n"
+        if type(drawable) is Line:
+            structure += "usemtl purple\n"
+            structure += "l " + str(old_counter) + " " + str(self.counter) + "\n"
+        if type(drawable) is Wireframe:
+            structure += "usemtl purple\n"
+            append_str = "l"
+            for i in range(old_counter, self.counter+1):
+                append_str += " " + str(i)
+            append_str += " " + str(old_counter) + "\n"
+            structure += append_str
+
+        return (vertices, structure)
 
     def write_all_display_file(self, file_name: str):
+        if file_name == "" or file_name == "." or file_name == "..":
+            file_name = "output.obj"
+        else:
+            file_name += ".obj"
+        self.reset_counter()
+        vertices = ""
+        structure = ""
+        for key in self.display_file.keys():
+            (v,s) = self.write_obj(key)
+            vertices += v
+            structure += s
         file = open(file_name, 'w')
-        file.write("# List of geometric vertices, with (x, y, z [,w]) coordinates, w is optional and defaults to 1.0.\n")
-        
-        # insert vertices
-        for obj in self.display_file.values():
-            self.write_obj(file_name, obj)
+        file.write(vertices)
+        file.write(structure)
         
         file.close()
--- pycg/tests/test_obj.py
+++ pycg/tests/test_obj.py
@@ -1,14 +1,43 @@
+# import os, sys
+
+# currentdir = os.path.dirname(os.path.realpath(__file__))
+# parentdir = os.path.dirname(currentdir)
+# sys.path.append(parentdir)
 
 
-from graphics import Line, Point
+
+from graphics import Line, Point, Wireframe
 from obj import ObjDescriptor
 
 
-def test_write_read():
+def test_write():
     obj = ObjDescriptor({"stop": Line(Point(0, 22), Point(0, 310))})
-    obj.write_obj("/home/arthur/INE5420-CG/podre.obj", "stop")
-    # with read()
+    write = obj.write_obj("stop")
+    assert write[0] == "v 0 22 0.0 1.0\nv 0 310 0.0 1.0\n"
+    assert write[1] == "o stop\nusemtl purple\nl 1 2\n"
 
+    obj = ObjDescriptor({"dude,STOP": Wireframe(Point(-575, 0),
+                                Point(-475, 135),
+                                Point(-220, 152),
+                                Point(-130, 300),
+                                Point(0, 0))})
+    write = obj.write_obj("dude,STOP")
+    assert write[0] == 'v -575 0 0.0 1.0\nv -475 135 0.0 1.0\nv -220 152 0.0 1.0\nv -130 300 0.0 1.0\nv 0 0 0.0 1.0\n'
+    assert write[1] == 'o dude,STOP\nusemtl purple\nl 1 2 3 4 5 1\n'
+
+    many_objs = ObjDescriptor({"dude,STOP": Wireframe(Point(-575, 0),
+                                Point(-475, 135),
+                                Point(-220, 152),
+                                Point(-130, 300),
+                                Point(0, 0)),
+                        "stop": Line(Point(0, 22), Point(0, 310)),
+                        "pointer": Point(0, 10)})
+
+    other = many_objs.write_obj("dude,STOP");
+    assert obj.write_obj("dude,STOP", True) == other
+
+    # write = many_objs.write_all_display_file("./output.obj")
 
     #TODO Finish reading files:
         #TODO read files, deal with path issue, create window to insert name in gui
+# test_write()
